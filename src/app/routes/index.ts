2ebc746e79989e1b3ece37b59e21c571d7e16be2
suzan <asadsuzan7@gmail.com>
2025-05-09 17:20:33 +0600

    feat(customer):add customer routes

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import { BikeRoute } from "../modules/Bike/bike.route";
+import { CustomerRoute } from "../modules/Customers/customer.route";
 
 const router = express.Router();
 
 const moduleRoutes = [
 
+  {
+    path: "/customers",
+    route: CustomerRoute,
+  },
+  
   {
     path: "/bikes",
     route: BikeRoute,
